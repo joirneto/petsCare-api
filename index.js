3dc6fbdb308546ae057b944dffd9a4c845671411
joirneto <joirneto@gmail.com>
2021-09-07 16:42:39 -0300

    Adicionando atendimentos

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas')
+
 const PORT = 3000;
 
 const app = customExpress()
 
-app.listen(PORT, () =>{
-  console.log(`Server listenig at PORT ${PORT}` )
+conexao.connect((erro)=>{
+  if(erro){
+    console.log("Erro: ", erro);
+  }else{
+    console.log("Conexão realizada com sucesso!");
+    Tabelas.init(conexao);
+    
+    app.listen(PORT, () =>{
+      console.log(`Server listenig at PORT ${PORT}` )
+    });
+  }
 });
+
+
